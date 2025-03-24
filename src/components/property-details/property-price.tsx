a7a394be47e235226e9be0966f45b65a06b1c01d
COUGRAGE <cougrage@COUGRAGEs-MacBook-Air.local>
2025-03-24 17:48:27 +0100

    made some changes

--- src/components/property-details/property-price.tsx
+++ src/components/property-details/property-price.tsx
@@ -1,11 +1,25 @@
+import { Property } from "../../types/property";
 interface PropertyPriceProps {
-  buy: boolean;
+  buy?: boolean; // Defines buy as an optional boolean prop
 }
 
-
 export function PropertyPrice({ buy }: PropertyPriceProps) {
+  // const formattedPrice = new Intl.NumberFormat("en-US", {
+  //   style: "currency",
+  //   currency: "USD",
+  //   minimumFractionDigits: 2,
+  //   maximumFractionDigits: 2,
+  // }).format(price.price);
+
   return (
     <div className="flex flex-col gap-5">
+      {/* <div className="bg-[#1a1a1a] p-6 rounded-lg space-y-4">
+        <div className="text-center">
+          <div className="text-gray-400">On Sale</div>
+          <div className="text-3xl font-bold">{formattedPrice}</div>
+        </div>
+      </div> */}
+
       <div className="bg-[#1a1a1a] p-6 rounded-lg space-y-4">
         <div className="space-y-2 flex flex-col gap-6">
           <div className="text-center text-gray-400">Invest in property</div>
